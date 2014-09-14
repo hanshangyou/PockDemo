926872d8e5d46bc15316e26d5b8afcbe9204ff58
runcrow <runcrow@runcrows-Mac.local>
2014-09-14 21:13:45 +0800

    Initial Commit

---
+++ PockDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PockDemo
+//
+//  Created by runcrow on 14-9-14.
+//  Copyright (c) 2014年 crowWorld. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ PockDemo/ViewController.m
@@ -0,0 +1,28 @@
+//
+//  ViewController.m
+//  PockDemo
+//
+//  Created by runcrow on 14-9-14.
+//  Copyright (c) 2014年 crowWorld. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+            
+
+@end
+
+@implementation ViewController
+            
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
